d3cc977fade6c8f5e35ee20052689a6a1b526d0c
liakh10 <119670997+liakh10@users.noreply.github.com>
2026-06-27 15:10:06 +0300

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -4,7 +4,7 @@ export const GAME_CONFIG = {
   network: "mainnet-beta" as const,
 };
 
-export const CA: string = "COMING SOON";           // Replace with real CA on launch
+export const CA: string = "4YnnaqTHv9rdyJLTzwGncXfJhTm3mo1SFUKh3VpVpump";           // Replace with real CA on launch
 export const X_URL = "https://x.com/soon";   // Replace with your X handle
 export const TICKER = "$TrenchBet";
 export const TOKEN_NAME = "TrenchBet";
